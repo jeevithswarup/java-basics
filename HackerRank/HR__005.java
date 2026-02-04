c71441c45c75bb978fa68212e8e683b0b059df84
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-02-04 08:46:56 +0530

    updated complete structure of my learning from diffirent platforms

--- HackerRank/HR__005.java
+++ HackerRank/HR__005.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class HR__005{
-    public static void main(String []argh){
+    public static void main(String []args){
         Scanner in = new Scanner(System.in);
         int t=in.nextInt();
         for(int i=0;i<t;i++){
@@ -16,6 +16,6 @@ class HR__005{
             }
             System.out.println();
         }
-        in.close();
+        
     }
 }
