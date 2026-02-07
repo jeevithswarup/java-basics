69076b9b17dd2049eea63deaae0350d2e221f694
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-02-07 08:56:52 +0530

    Added LC66 & LCC 88

--- LeetCode/LC66.java
+++ LeetCode/LC66.java
@@ -3,16 +3,17 @@ public class LC66 {
     public void PlusOne() {
         int[] arr = new int[] {3,4,5,9};
          int[] arr2 = new int[arr.length + 1];
-        if (arr.length == 1 && arr[0] < 9) {
-            arr[0] += 1;
-        } else if (arr[arr.length - 1] == 9) {
+        // if (arr.length == 1 && arr[0] < 9) {
+        //     arr[0] += 1;
+        // } else
+         if (arr[arr.length - 1] == 9) {
             for (int i = 0; i < arr.length; i++) {
                 arr2[i] = arr[i];
             }
             arr2[arr.length - 1] = 1;
-        } else {
-            arr[arr.length - 1] += 1;
-        }
+        // } else {
+        //     arr[arr.length - 1] += 1;
+         }
         System.out.println(Arrays.toString(arr2));
     }
 
