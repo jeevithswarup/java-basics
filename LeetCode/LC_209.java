43fecaccc4a6e74bb1e756c9e67831c3d223c2ea
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-03-17 22:02:16 +0530

    LEETCODE  219

---
+++ LeetCode/LC_209.java
@@ -0,0 +1,9 @@
+public class LC_209 {
+    public int minSubArrayLen(int target, int[] nums) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ LeetCode/LC_219.java
@@ -0,0 +1,14 @@
+
+
+public class LC_219 {
+    public int containsNearbyDuplicate(int[] nums, int k) {
+    
+        return sum;
+    }
+    public static void main(String[] args) {
+        LC_219 obj = new LC_219();
+        int[] nums = new int[] {1,2,3,9};
+        int k = 3;
+     System.out.println(obj.containsNearbyDuplicate(nums, k));
+     }
+}
