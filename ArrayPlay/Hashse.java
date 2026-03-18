73c86a9adbc4e64315d2106c4fad36344974e445
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-03-18 14:09:14 +0530

    LEETCODE  003

--- ArrayPlay/Hashse.java
+++ ArrayPlay/Hashse.java
@@ -4,16 +4,21 @@ public class Hashse {
     public int UniqueHash(int[] arr) {
        
         HashSet<Integer> set = new HashSet<>();
-        for (int i : arr) {
-            if (!set.add(i))
-                return i;
+        int left = 0;
+        int max_length = 0;
+        for (int right = 0; right < arr.length;right++) {
+            while (!set.add(arr[right])) {
+                set.remove(arr[left]);
+                left++;
 
+            }
+            max_length = Math.max(max_length,set.size());
         }
-        return -1;
+        return max_length;
     }
 public static void main(String[] args) {
     Hashse obj = new Hashse();
-    int[] arr = new int[] { 1, 2, 2, 3, 4, 4 };
+    int[] arr = new int[] {1, 2, 3, 2, 4};
     System.out.println(obj.UniqueHash(arr));
 }
 }
---
+++ LeetCode/LC_003.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+
+public class LC_003{
+
+    public int Long_substring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int left = 0;
+        int max_Len = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+            while (!set.add(ch)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            max_Len = Math.max(max_Len, right - left + 1);
+        }
+        return max_Len;
+
+    }
+
+    public static void main(String[] args) {
+        LC_003 obj = new LC_003();
+        String s = new String("abcabcbb");
+        obj.Long_substring(s);
+       
+    }
+    
+}
