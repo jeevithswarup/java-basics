21afc40a95d8fd4c44561ef0f595968ee35b0a86
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-03-19 08:40:26 +0530

    Leetcode 713

--- LeetCode/LC_713.java
+++ LeetCode/LC_713.java
@@ -1,3 +1,15 @@
-public class LC_713{
+public class LC_713 {
+
+    public int numSubarrayProduct(int [] nums,k) {
+
+
+        return 0;
+    }
+  public static void main(String[] args) {
+      LC_713 obj = new LC_713();
+      int[] nums = new int[] { 10, 5, 2, 6 };
+      int k = 100;
+      obj.numSubarrayProduct(nums,k);
+  }
     
 }
