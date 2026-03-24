f3b4b8b9547cc3bd9e189a0ed0c227c09c78409f
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-03-24 10:03:53 +0530

    learning HashMap

--- ArrayPlay/Hashmap.java
+++ ArrayPlay/Hashmap.java
@@ -2,19 +2,17 @@ import java.util.HashMap;
 public class Hashmap {
     
     public int HashCheck(int[] nums) {
-        int j=1;
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0;i<nums.length-1;i++){
-            map.put(nums[i], nums[j]);
-            j++;
+        for (int x : nums) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
         }
         System.out.println(map);
-        return 0;
-        
+
+      return 0;
     }
     public static void main(String[] args) {
         Hashmap obj = new Hashmap();
         int[] nums = new int[] { 1, 2, 2, 3, 1 };
-        System.out.println(obj.HashCheck(nums));
+        obj.HashCheck(nums);
     }
 }
