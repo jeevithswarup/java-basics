840cda6f9917fc365b6009dcf05c8d17e2d8db89
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-03-24 15:22:28 +0530

    String Frequency and Max_Frequency using HashMap

--- StringFrequency.java
+++ StringFrequency.java
@@ -3,18 +3,21 @@ import java.util.HashMap;
 public class StringFrequency {
     public int Frequency(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
+        int max_freq = 0;
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }System.out.println(map);
+            max_freq = Math.max(max_freq, map.get(ch));
+        }
+        System.out.println(map);
 
-        return 0;
+        return max_freq;
     }
 
     public static void main(String[] args) {
         StringFrequency obj = new StringFrequency();
         String s = "aabbbcc";
-        obj.Frequency(s);
+       System.out.println(obj.Frequency(s));
 
     }
 }
