0107ff134cc1ab30c0ddcfcff4520a543cb16910
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-03-25 20:05:14 +0530

    LC_930

--- LeetCode/LC_560.java
+++ LeetCode/LC_560.java
@@ -5,17 +5,16 @@ public class LC_560 {
         HashMap<Integer,Integer>map=new HashMap<>();
         int sum = 0;
         int count = 0;
-        int value=0;
+      
+        map.put(0, 1);
         for (int i = 0; i < arr.length; i++) {
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
             sum += arr[i];
-            value = sum - k;
-            if (map.containsKey(value)) {
-                count+= map.get(value);
+            if (map.containsKey( sum - k)) {
+                count += map.get( sum - k);
             }
+        map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return count;
-
     }
     public static void main(String[] args) {
         LC_560 obj = new LC_560();
--- LeetCode/LC_930.java
+++ LeetCode/LC_930.java
@@ -1,30 +1,25 @@
 public class LC_930 {
 
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int left = 0;
-        int sum = 0;
+      
         int count = 0;
-        for (int right = 0; right < nums.length; right++) {
-            sum += nums[right];
-
-            while (sum > goal) {
-                sum = sum - nums[left];
-                left++;
+        for (int i = 0; i < nums.length; i++) {
+              int sum=0;
+            for (int j = i; j < nums.length; j++) {
+                   sum+=nums[j];
+                if ( sum== goal) {
+                 
+                    count++;
+                }
             }
-            if (sum == goal) {
-                count++;
-            }
-
         }
-        if (sum == goal) {
-            count++;
-        }
-        return count;
+         return count;
     }
+
     public static void main(String[] args) {
         LC_930 obj = new LC_930();
-        int[] nums=new int[]{1,0,1,0,1};
-        int goal=2;
+        int[] nums=new int[]{0,0,0,0,0};
+        int goal=0;
         System.out.println(obj.numSubarraysWithSum(nums, goal));
     }
 }
