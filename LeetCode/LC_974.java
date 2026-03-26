b22904b3f9f59f29d915ba2947f09f7336eb764d
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-03-26 21:21:56 +0530

    LC_974

--- LeetCode/LC_974.java
+++ LeetCode/LC_974.java
@@ -1,3 +1,28 @@
+import java.util.HashMap;
+
 public class LC_974 {
-    
+    public int subarraysDivByK(int[] nums, int k) {
+        int sum = 0;
+        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            int rem = sum % k;
+            if (rem < 0) {
+                rem += k;
+            }
+            if (map.containsKey(rem)) {
+                count += map.get(rem);
+            }
+            map.put(rem, map.getOrDefault(rem, 0) + 1);
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        LC_974 obj = new LC_974();
+        int[] nums = new int[] { 4, 5, 0, -2, -3, 1 };
+        int k = 5;
+        
+    }
 }
