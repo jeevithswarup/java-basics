45fdd304af76f26145d207e000660842a0576ce9
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-03-27 15:36:41 +0530

    LC_1590

--- LeetCode/LC_1590.java
+++ LeetCode/LC_1590.java
@@ -3,35 +3,35 @@ import java.util.HashMap;
 
 public class LC_1590 {
     
-    public int minSubarray(int[] nums, int p) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int sum = 0;
-        map.put(0, -1);
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
+   public int minSubarray(int[] nums, int p) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int totalsum=0;
+        int sum=0;
+        int min_len=nums.length;
+        map.put(0,-1);
+        for(  int x:nums){
+            sum=(sum+x)%p;
         }
-            int target = sum % p;
-            if (target == 0) {
-                return 0;
+        if(sum==0){
+            return 0;
         }
-        sum = 0;
-        int count = 0;
-        int min_len = Integer.MAX_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            if (map.containsKey(sum - target)) {
-                int length = i - (map.get(sum - target));
-                min_len = Math.min(min_len, length);
-            } else {
-                map.put(sum, i);
+        int k=sum%p;
+        sum=0;
+        for( int i=0;i<nums.length;i++){
+            sum=(sum+nums[i])%p;
+        
+            int target=(sum-k+p)%p;
+            if(map.containsKey(target)){
+                min_len=Math.min(min_len,i-map.get(target));
             }
-        }
-        if (min_len > 0) {
-            return min_len;
-        }else{
+            map.put(sum,i);
+        }   
+        if(min_len==nums.length){
             return -1;
         }
-    }
+        return min_len;
+             
+}
     public static void main(String[] args) {
         LC_1590 obj = new LC_1590();
         int[] nums = new int[] {6,3,5,2};
