ffed3904302ea0e9b05ba8144614fff13c47ba83
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-03-27 13:20:00 +0530

    LC_1590

--- LeetCode/LC_1590.java
+++ LeetCode/LC_1590.java
@@ -1,7 +1,42 @@
+
+import java.util.HashMap;
+
 public class LC_1590 {
     
-  public int minSubarray(int[] nums, int p) {
-        
+    public int minSubarray(int[] nums, int p) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        map.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+            int target = sum % p;
+            if (target == 0) {
+                return 0;
+        }
+        sum = 0;
+        int count = 0;
+        int min_len = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - target)) {
+                int length = i - (map.get(sum - target));
+                min_len = Math.min(min_len, length);
+            } else {
+                map.put(sum, i);
+            }
+        }
+        if (min_len > 0) {
+            return min_len;
+        }else{
+            return -1;
+        }
+    }
+    public static void main(String[] args) {
+        LC_1590 obj = new LC_1590();
+        int[] nums = new int[] {1,2,3};
+        int p = 6;
+        System.out.println(obj.minSubarray(nums, p));
     }
 
 }
