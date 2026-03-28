b54701d1e5685b295a0ce1d48a52e5bf0a35fc1a
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-03-28 11:03:03 +0530

    LC_724

--- LeetCode/LC_724.java
+++ LeetCode/LC_724.java
@@ -1,11 +1,29 @@
 public class LC_724 {
     public int pivotIndex(int[] nums) {
+        int sum=0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        int leftsum = 0;
+            
+        for (int j = 0; j < nums.length; j++) {   
+            sum -= nums[j];
 
-        return 0;
+            if (leftsum == sum) {
+                return j;
+            }
+
+            leftsum += nums[j];
+          
+        }
+
+        return -1;
     }
 
     public static void main(String[] args) {
-
+        LC_724 obj = new LC_724();
+        int[] nums = new int[] { 1, 7, 3, 6, 5, 6 };
+       System.out.println(obj.pivotIndex(nums));
     }
 }
 
