ac0d93f41e9981fc3c4d74e94d1ba3eaa6ce9048
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-03-28 09:52:24 +0530

    LC_303

--- LeetCode/LC_303.java
+++ LeetCode/LC_303.java
@@ -1,3 +1,25 @@
 public class LC_303 {
+   
+    public int[] NumArray(int[] nums) {
+        int[] prefix = new int[nums.length];
+        int prefixSum = 0;
+        for (int i=0;i<nums.length;i++){
+            prefix[i] = prefixSum + nums[i];
+        }
+        return prefix;
+    }
     
+    public int sumRange(int left, int right) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        LC_303 obj = new LC_303();
+    int[] nums = new int[] { -2, 0, 3, -5, 2, -1 };
+    int left=0;
+    int right=2;
+    obj.NumArray(nums);
+    obj.sumRange(left, right);
+
+}
 }
