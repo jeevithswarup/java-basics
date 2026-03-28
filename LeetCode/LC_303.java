f2561498e4b5da66dc78a580ee03a0be4506db13
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-03-28 10:48:05 +0530

    LC_303

--- LeetCode/LC_303.java
+++ LeetCode/LC_303.java
@@ -1,16 +1,22 @@
+
 public class LC_303 {
    
     public int[] NumArray(int[] nums) {
         int[] prefix = new int[nums.length];
         int prefixSum = 0;
-        for (int i=0;i<nums.length;i++){
-            prefix[i] = prefixSum + nums[i];
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum += nums[i];
+            prefix[i] = prefixSum;
         }
         return prefix;
     }
     
-    public int sumRange(int left, int right) {
-        return 0;
+    public int sumRange(int[] prefix, int left, int right) {
+        if (left == 0) {
+            return prefix[right];
+        }else{
+            return prefix[right]-prefix[left-1];
+        }
     }
 
     public static void main(String[] args) {
@@ -18,8 +24,8 @@ public class LC_303 {
     int[] nums = new int[] { -2, 0, 3, -5, 2, -1 };
     int left=0;
     int right=2;
-    obj.NumArray(nums);
-    obj.sumRange(left, right);
+   
+   System.out.println(obj.sumRange( obj.NumArray(nums),left, right));
 
 }
 }
