850d681d83e6e7eb0152abf5853c62039394e305
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-03-29 10:25:58 +0530

    LC_11

---
+++ LeetCode/LC_11.java
@@ -0,0 +1,25 @@
+public class LC_11 {
+     
+    
+    public int Max_Area(int arr[]) {
+        int left = 0;
+        int right = arr.length - 1;
+        int max_area = 0;
+
+        while (left < right) {
+            int min_height = Math.min(arr[left], arr[right]);
+            int width = right - left;
+            int area = min_height * width;
+            max_area = Math.max(max_area, area);
+            if (arr[left] < arr[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max_area;
+    }
+    public static void main(String[] args) {
+        LC
+    }
+}
