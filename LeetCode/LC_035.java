8526f90cb03117dc68a893dc3bae44e00bb80222
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-03-31 20:40:54 +0530

    LC_035

--- LeetCode/LC_035.java
+++ LeetCode/LC_035.java
@@ -6,7 +6,12 @@ public class LC_035 {
         int low = 0;
         int high = nums.length - 1;
         while (low <= high) {
-            
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            else id    
+            }
         }
         return 0;
     }
