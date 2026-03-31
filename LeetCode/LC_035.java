8a766568193a5a0147a677b7387316dcba1b8e75
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-03-31 20:38:28 +0530

    LC_035

--- LeetCode/LC_035.java
+++ LeetCode/LC_035.java
@@ -1,4 +1,15 @@
+
+
 public class LC_035 {
-    
+
+    public int searchInsert(int []nums,int target){
+
+        return 0;
+    }
+  public static void main(String[] args) {
+      LC_035 obj = new LC_035();
+      int[] nums = new int[] { 1, 3, 5, 6 };
+      int target = 7;
+  }
     
 }
