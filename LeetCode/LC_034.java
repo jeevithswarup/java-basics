2edb056f44691ba28d6c56f41beb5db0af79e985
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-04-03 22:39:30 +0530

    LC_034

--- LeetCode/LC_034.java
+++ LeetCode/LC_034.java
@@ -1,30 +1,50 @@
 import java.util.Arrays;
 
 public class LC_034{
-public int[] searchRange(int[] nums, int target) {
-        int low=0;
-        int high=nums.length-1;
-        int[] result=new int[]{-1,-1};
-        while (low < high) {
+  public int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return new int[]{first, last};
+    }
+
+    private int findFirst(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
             int mid = (low + high) / 2;
-            if (nums[mid] == target && nums[mid - 1] == target) {
-                result[0] = mid;
-                result[1] = mid - 1;
-                return result;
-            } else if (nums[mid] == target && nums[mid + 1] == target) {
-                result[0] = mid;
-                result[1] = mid + 1;
-                return result;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                high = mid - 1; // move left
             } else if (nums[mid] < target) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
-
         }
-        return result;
-
+        return ans;
     }
+
+    private int findLast(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                low = mid + 1; // move right
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         LC_034 obj = new LC_034();
         int[] nums = new int[] { 5, 7, 7, 8, 8, 10 };
