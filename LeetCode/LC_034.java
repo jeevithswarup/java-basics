deef704cde27804fe73c6b570c8bcc0394018382
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-04-03 22:27:31 +0530

    LC_034

--- LeetCode/LC_034.java
+++ LeetCode/LC_034.java
@@ -5,18 +5,21 @@ public class LC_034{
 public int[] searchRange(int[] nums, int target) {
         int low=0;
         int high=nums.length-1;
-
+        int[] result=new int[2];
        while(low<high){
         int mid=(low+high)/2;
-        if(nums[mid]==target && nums[mid-1]==target){
-            return  
+        if (nums[mid] == target && nums[mid - 1] == target) {
+            result[0]=
+            return  re;
         }
        } 
 
     }
     public static void main(String[] args) {
         LC_034 obj = new LC_034();
-        int[] nums = new int[] {};
+        int[] nums = new int[] { 5, 7, 7, 8, 8, 10 };
+        int target = 8;
+       System.out.println(obj.searchRange(nums, target));
     }
     
 }
