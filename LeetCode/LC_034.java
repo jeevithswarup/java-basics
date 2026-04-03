d0893c5bd3d231d08b6d4dea83f925f63bec45eb
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-04-03 22:33:25 +0530

    LC_034

--- LeetCode/LC_034.java
+++ LeetCode/LC_034.java
@@ -1,34 +1,35 @@
-
-import com.sun.tools.javac.Main;
+import java.util.Arrays;
 
 public class LC_034{
 public int[] searchRange(int[] nums, int target) {
         int low=0;
         int high=nums.length-1;
-        int[] result=new int[2];
-       while(low<high){
-        int mid=(low+high)/2;
-        if (nums[mid] == target && nums[mid - 1] == target) {
-            result[0] = mid;
-            result[1] = mid - 1;
-            return result;
-        } else if (nums[mid] == target && nums[mid + 1] == target) {
-            result[0] = mid;
-            result[1] = mid + 1;
-        } else if (nums[mid] < target) {
-            low = mid + 1;
-        } else {
-            high = mid - 1;
+        int[] result=new int[]{-1,-1};
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target && nums[mid - 1] == target) {
+                result[0] = mid;
+                result[1] = mid - 1;
+                return result;
+            } else if (nums[mid] == target && nums[mid + 1] == target) {
+                result[0] = mid;
+                result[1] = mid + 1;
+                return result;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+
         }
-        
-       } 
+        return result;
 
     }
     public static void main(String[] args) {
         LC_034 obj = new LC_034();
         int[] nums = new int[] { 5, 7, 7, 8, 8, 10 };
-        int target = 8;
-       System.out.println(obj.searchRange(nums, target));
+        int target = 2;
+       System.out.println(Arrays.toString(obj.searchRange(nums, target)));
     }
     
 }
