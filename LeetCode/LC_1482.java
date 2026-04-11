276ef7402996182ac0cc79bc598d77559f293e89
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-04-11 09:29:47 +0530

    LC

--- LeetCode/LC_1482.java
+++ LeetCode/LC_1482.java
@@ -4,6 +4,10 @@ public class LC_1482 {
 
     }
     public static void main(String[] args) {
-        
+        LC_1482 obj = new LC_1482();
+        int[] bloomDay = new int[] { 1, 10, 3, 10, 2 };
+        int m = 3;
+        int k = 1;
+        obj.minDays(bloomDay, m, k);
     }
 }
