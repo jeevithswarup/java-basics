2766a22f517324cac225988eb48b995fe76238e1
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-04-12 08:34:30 +0530

    LC

--- LeetCode/LC_1283.java
+++ LeetCode/LC_1283.java
@@ -1,8 +1,9 @@
 public class LC_1283 {
-    int total_sum = 0;
+   
     
 
     public int smallestDivisor(int[] nums, int threshold) {
+        if(threshold < nums.length) return -1;
         int low = 1;
         int high = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -11,7 +12,7 @@ public class LC_1283 {
         while (low <= high) {
             int mid = (low + high) / 2;
 
-            if (divisorValue(nums, threshold, mid) <= threshold) {
+            if (divisorValue(nums, mid) <= threshold) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
@@ -20,10 +21,10 @@ public class LC_1283 {
 
         return low;
     }
-    public int divisorValue(int[] nums, int threshold, int mid) {
-   
+    public int divisorValue(int[] nums, int mid) {
+    int total_sum = 0;
         for (int x : nums) {
-            total_sum += (int)Math.ceil((double)x / mid);
+            total_sum +=  (x + mid - 1) / mid; 
          }
         return total_sum;
     }
