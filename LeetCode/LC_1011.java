df779599d3c708a05b3bb5287d1a58d7ebdf1949
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-04-12 14:15:42 +0530

    LC

---
+++ LeetCode/LC_1011.java
@@ -0,0 +1,23 @@
+public class LC_1011 {
+
+
+    public int shipWithinDays(int[] weights, int days) {
+        int low = 0;
+        int high = 0;
+        for (int x : weights) {
+            low = Math.max(low, x);
+            high += x;
+        }
+        System.out.println(low+" "+high);
+
+
+        return 0;
+    }
+    public static void main(String[] args) {
+        LC_1011 obj = new LC_1011();
+        int[] weights = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int days = 5;
+        obj.shipWithinDays(weights, days);
+    }
+}
+
