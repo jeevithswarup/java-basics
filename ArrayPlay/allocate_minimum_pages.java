aef9397ffe90b6752df5e21ed31643a7ed498d81
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-04-14 11:18:42 +0530

    LC

--- ArrayPlay/allocate_minimum_pages.java
+++ ArrayPlay/allocate_minimum_pages.java
@@ -2,13 +2,33 @@ public class allocate_minimum_pages {
     
 
     public int findPages(int[] arr, int k) {
+        for (int x : arr) {
+            
+        }
 
         return 0;
 
     }
     
     public boolean isValid(int[] arr, int k, int max_pages) {
-        return true;
+        int students = 1;
+        int countPages = 0;
+
+        for (int pages : arr) {
+            if (countPages + pages <= max_pages) {
+                countPages += pages;
+            } else {
+                students++;
+                countPages = pages;
+            }
+            if (students >= k) {
+                return true;
+            }
+
+        }
+
+
+        return false ;
     }
     public static void main(String[] args) {
         allocate_minimum_pages obj   = new allocate_minimum_pages();
