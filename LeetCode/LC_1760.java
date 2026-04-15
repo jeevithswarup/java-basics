343bb1955d1fc33e94211cd334788f90afc216a6
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-04-15 10:29:50 +0530

    LC

--- LeetCode/LC_1760.java
+++ LeetCode/LC_1760.java
@@ -7,13 +7,16 @@ public class LC_1760 {
             high = Math.max(high, x);
         }
         while (low <= high) {
-            
+
             int max_balls = low + (high - low) / 2;
+
+            if (isValid(nums, maxOperations, max_balls)) {
+                high = max_balls - 1;
+            } else {
+                low = max_balls + 1;
+            }
         }
-
-    
-
-        return 0;
+        return ;
     }
 
     public boolean isValid(int[] nums, int maxOperations, int max_balls) {
