9fcf84233056d3604ee8527714e37cb0e963c8f7
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-04-15 10:14:12 +0530

    LC

--- LeetCode/LC_1760.java
+++ LeetCode/LC_1760.java
@@ -1,3 +1,18 @@
 public class LC_1760 {
     
+    public int minimumSize(int[] nums, int maxOperations) {
+
+        return 0;
+    }
+
+    public boolean isValid(int[] nums, int maxOperations,int max_balls) {
+        
+
+    } 
+
+
+
+    public static void main(String[] args) {
+        
+    }
 }
