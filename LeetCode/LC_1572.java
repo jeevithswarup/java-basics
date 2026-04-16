ad1579f2b10f19cbcf67ea4cb24200333ba14ee1
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-04-16 21:17:31 +0530

    LC

--- LeetCode/LC_1572.java
+++ LeetCode/LC_1572.java
@@ -2,8 +2,11 @@ public class LC_1572 {
     public int diagonalSum(int[][] mat) {
         int sum=0;
         for (int i = 0; i < mat.length; i++) {
-            
-            sum += mat[i][i] + mat[i][mat[i].length - i-1];
+            if (i == mat[i].length - i - 1) {
+                sum += mat[i][i];
+            } else {
+                sum += mat[i][i] + mat[i][mat[i].length - i - 1];
+            }
         }
 
         return sum;
