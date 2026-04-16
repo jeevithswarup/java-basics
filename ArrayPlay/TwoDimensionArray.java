f415e458c04db91063684e0cf0723daab1a46941
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-04-16 20:18:24 +0530

    LC

--- ArrayPlay/TwoDimensionArray.java
+++ ArrayPlay/TwoDimensionArray.java
@@ -1,3 +1,23 @@
-public TwoDimensionArray {
+public class TwoDimensionArray {
     
+    public int MatrixSum(int[][] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                sum += nums[i][j];
+            }
+        }
+
+
+
+        return sum;
+    }
+    public static void main(String[] args) {
+        TwoDimensionArray obj = new TwoDimensionArray();
+        int [][] nums= new int[][] {  {1, 2, 3},
+                                      {4, 5, 6},
+                                      {7, 8, 9}
+                                    };
+        System.out.println(obj.MatrixSum(nums));
+    }
 }
