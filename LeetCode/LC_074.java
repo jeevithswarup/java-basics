68ce95ec99bf2ac152598ca4c006d6e6ae3c6882
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-04-16 21:13:27 +0530

    LC

---
+++ LeetCode/LC_074.java
@@ -0,0 +1,18 @@
+public class LC_074 {
+    
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+      
+
+        return false;
+    }
+    
+    public int binarySearch(int[][] matrix,int target){
+            for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][i] <= target && target <= matrix[i][matrix[i].length - 1]) {
+                 
+            }
+        }
+    }
+}
---
+++ LeetCode/LC_1572.java
@@ -0,0 +1,19 @@
+public class LC_1572 {
+    public int diagonalSum(int[][] mat) {
+        int sum=0;
+        for (int i = 0; i < mat.length; i++) {
+            
+            sum += mat[i][i] + mat[i][mat[i].length - i-1];
+        }
+
+        return sum;
+    }
+    public static void main(String[] args) {
+        LC_1572 obj = new LC_1572();
+        int[][] mat=new int[][]{{1,2,3},
+                                {4,5,6},
+                                {7,8,9}
+        };
+     System.out.println(obj.diagonalSum(mat));
+    }
+}
