346257a62b019c2f7f9768e1827a7b51de2b2fef
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-05-19 16:03:29 +0530

    removeelement in Lineked List

--- LinkedList/CreationLL.java
+++ LinkedList/CreationLL.java
@@ -11,7 +11,87 @@ public class CreationLL {
         }
     }
 
-    public void Print(Node head) {
+    Node head;
+    Node tail;
+    
+    public void creation(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+    }
+
+    private Node removeHead() {
+        if (head == null)
+            return head;
+        head = head.next;
+        if (head == null) {
+            head = null;
+            tail = null;
+        }
+        return head;
+
+    }
+    
+    private Node insertAtPosiNode(int data, int k) {
+        Node newNode = new Node(data);
+
+        if (k == 1) {
+            newNode.next = head;
+            head = newNode;
+        }
+        if (tail == null) {
+            tail = newNode;
+        }
+        Node temp = head;
+        int count = 1;
+        while (temp.next != null && count < k - 1) {
+
+            temp = temp.next;
+            count++;
+        }
+
+        if (temp.next == null) {
+            System.out.println("Invalid Location");
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+        if (newNode.next == null) {
+            tail = newNode;
+        }
+        return head;
+    }
+    
+    private Node removeTail() {
+
+        if (head == null)
+            return null;
+
+        Node temp = head;
+        if (temp.next == null) {
+            head = null;
+            tail = null;
+        }
+
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = null;
+        tail = temp;
+
+        return head;
+    }
+    
+
+
+    public void Print() {
         Node temp = head;
         while (temp != null) {
             System.out.print(temp.data + "--->");
@@ -21,13 +101,18 @@ public class CreationLL {
     }
 
     public static void main(String[] args) {
+        
         CreationLL obj = new CreationLL();
-        Node n1 = new Node(10);
-        Node n2 = new Node(20);
-        Node n3 = new Node(30);
-        n1.next = n2;
-        n2.next = n3;
-        Node head = n1;
-       obj.Print(head);
+        obj.creation(10);
+        obj.creation(20);
+        obj.creation(30);
+        obj.creation(40);
+        obj.Print();
+        obj.removeHead();
+        obj.Print();
+        obj.insertAtPosiNode(55, 3);
+        obj.Print();
+        obj.removeTail();
+        obj.Print();
     }
 }
--- LinkedList/InsertionsLL.java
+++ LinkedList/InsertionsLL.java
@@ -1,7 +1,4 @@
 public class InsertionsLL {
-    
-
-
     static class Node {
         int data;
         Node next;
@@ -31,19 +28,76 @@ public class InsertionsLL {
         if (head == null)
             return head;
         head = head.next;
+        if (head == null) {
+            tail = null;
+        }
         return head;
-        
+
+    }
+
+    private void insertAtPosition(int k, int data) {
+
+        Node newnode = new Node(data);
+
+        if (k == 1) {
+            newnode.next = head;
+            head = newnode;
+
+            if (tail == null) {
+                tail = newnode;
+            }
+            return;
+        }
+
+        Node temp = head;
+        int count = 1;
+
+        while (temp.next != null && count < k) {
+            temp = temp.next;
+            count++;
+        }
+
+        if (temp == null) {
+            System.out.println("invalid Location");
+            return;
+        }
+        newnode.next = temp.next;
+        temp.next = newnode;
+            if (newnode.next == null) {
+        tail = newnode;
+    }
+
+    }
+     
+    
+
+    private Node removeTail() {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            head = null;
+            tail = null;
+            return head;
+        }
+
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        temp.next = null;
+        tail = temp;
+        return head;
     }
 
     private void Print() {
         Node temp = head;
-        while (temp != null) {
+        while (temp!= null) {
             System.out.print(temp.data + "---->");
             temp = temp.next;
         }
         System.out.println("Null");
     }
-    
 
 
     public static void main(String[] args) {
@@ -51,9 +105,13 @@ public class InsertionsLL {
         obj.insertatend(10);
         obj.insertatend(20);
         obj.insertatend(30);
-        obj.insertatend(50);
+        obj.insertatend(40);
         obj.Print();     //normal linked list
         obj.removeHead();
-        obj.Print();     // Removed Head
+        obj.Print(); // Removed Head
+        obj.removeTail();
+        obj.Print();
+        obj.insertAtPosition(3, 99);
+        obj.Print();
     }
 }
