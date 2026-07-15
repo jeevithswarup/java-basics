819401c368a44067b677d74a08274f2f239598bb
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-07-15 11:20:18 +0530

    Add LeetCode 901 solution

--- Stack&Queue/NextSmallerIndex.java
+++ Stack&Queue/NextSmallerIndex.java
@@ -1,3 +1,34 @@
+import java.util.Arrays;
+import java.util.Stack;
 public class NextSmallerIndex {
     
+    public int [] Nse(int[] arr) {
+        int[] res = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            
+            
+            while (!st.isEmpty() && arr[i] >= arr[st.peek()-1]) {
+                st.pop();
+                
+            }
+
+            if (st.isEmpty()) {
+                res[i] = arr.length;
+            } else {
+                res[i] =i;
+            }
+
+            st.push(i);
+        }
+            
+
+        
+        return res;
+    }
+    public static void main(String[] args) {
+        NextSmallerIndex obj = new NextSmallerIndex();
+        int[] arr = new int[] { 3, 1, 2, 4 };
+        System.out.println(Arrays.toString(obj.Nse(arr)));
+    }
 }
